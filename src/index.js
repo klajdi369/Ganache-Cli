55182f4765e8b581291533aaba2846c0ecf0824e
klajdi369 <21219484+klajdi369@users.noreply.github.com>
2020-08-26 20:06:49 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+var http = require("http");
+const { spawn } = require("child_process");
+
+let port = process.env.PORT || 3000;
+const spawnGanache = spawn("ganache-cli", ["-b 9", "-n", "-u 0", "-p " + port]);
+
+console.log(port);
+
+let output;
+spawnGanache.stdout.on("data", (data) => {
+  console.log(`stdout: ${data}`);
+  output += data;
+});
+
+//create a server object:
+http
+  .createServer(function (req, res) {
+    res.write(output); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(8080); //the server object listens on port 8080

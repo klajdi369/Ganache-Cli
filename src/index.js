d08b8fe91c251f72e09be9fa25b4133633ff9d99
klajdi369 <21219484+klajdi369@users.noreply.github.com>
2020-10-12 22:53:31 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var http = require("http");
 const { spawn } = require("child_process");
 
 let port = process.env.PORT || 3000;
-const spawnGanache = spawn("ganache-cli", ["-b 9 -h 127.0.0.1", "-n", "-u 0", "-p " + port]);
+const spawnGanache = spawn("ganache-cli", ["-b 9 -h 0.0.0.0", "-n", "-u 0", "-p " + port]);
 
 console.log(port);
 
